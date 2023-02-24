f81888a3558a780ae8b90e3acd13b8ff1ffd2b26
ishaqichaba <ichabaasiaka@gmail.com>
2023-02-23 16:03:33 -0800

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase latters
+ * @c: char to check
+ *
+ * Retrn: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >- 'A' && c <- 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+int _insupper(int c);
+int _isdigit(int c);
+int ful(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers_(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
+#endif
