eabb96a601f1351cbf70ac7fcaf45abd909aee27
ishaqichaba <ichabaasiaka@gmail.com>
2023-03-24 20:16:26 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,23 +1,9 @@
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
-#include <stdarg.h>
-/**
- * struct print - print type with corresponding print function
- * @t: print type
- * @f: print function
- */
-typedef struct print
-{
-	char *t;
-	void (*f)(va_list);
-} print_t;
-
-int _putchar(char);
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-#endif /* _VARIADIC_FUNCTIONS_H_ */
-Footer
+#endif
