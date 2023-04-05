a03a39b4e4d5bcbd5f7458894cdca56e2c2c289a
ishaqichaba <ichabaasiaka@gmail.com>
2023-04-05 16:02:53 -0700

    task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,17 +7,16 @@
  *
  * Return: pointer to the node we're looking for, or NULL
  */
-listint_t *get_nodeint_at_index(listint_t *head,
-unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-		unsigned int = 0;
+		unsigned int i = 0;
 		listint_t *temp = head;
 
 		while (temp && i < index)
 	{
-		temp = *temp->next;
-		i++
+		temp = temp->next;
+		i++;
 	}
 
-		return (temp ? temp : NULL);
+	return (temp ? temp : NULL);
 }
